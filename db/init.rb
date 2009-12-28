ca72c2d68f4e23dec35276728a7776c396d1ee12
Alex Bird <alexebird@gmail.com>
2009-12-27 22:22:27 -0600

    Added logger.

--- db/init.rb
+++ db/init.rb
@@ -1,7 +1,9 @@
 # Here goes the database connection and options:
 require 'sequel'
+require 'logger'
 DB_ROOT = File.expand_path(File.dirname(__FILE__))
 DB = Sequel.connect("sqlite://#{DB_ROOT}/food.db")
+DB.loggers << Logger.new(STDOUT)
 
 # Here go the required models:
 require DB_ROOT + '/model/food'
